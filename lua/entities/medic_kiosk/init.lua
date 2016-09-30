973b74d2a15dbb844dec4edc6ed6ee132cedfede
Cody Fields <contact@leethecoder.com>
2016-09-29 23:55:16 -0400

    Added check to see if player has enough money

--- lua/entities/medic_kiosk/init.lua
+++ lua/entities/medic_kiosk/init.lua
@@ -20,21 +20,24 @@ function ENT:Initialize()
 	end
 end
 
-
-function ENT:Destruct()
-	self:GetPos()
-	util.Effect("Explosion", effectdata)
-end
-
 function ENT:Use( activator, caller )
-    if (caller:Health() > 99 and caller:IsPlayer()) then
-			caller:ChatPrint("You already have full health.")
-		else
+if (caller:Health() > 99 and caller:IsPlayer()) then
+	caller:ChatPrint("You already have full health.")
+	else
+		if (caller:getDarkRPVar("money") >  500) then
 			caller:addMoney(500)
 			caller:SetHealth(100)
 			self:Getowning_ent():addMoney(500)
 			self:EmitSound("items/smallmedkit1.wav")
+	else
+			caller:ChatPrint("You don't have enough money.")
 		end
+	end
+end
+
+function ENT:Destruct()
+	self:GetPos()
+	util.Effect("Explosion", effectdata)
 end
 
 function ENT:Explode()
@@ -57,32 +60,6 @@ function ENT:OnTakeDamage(dmg)
 end
 
 function ENT:Think()
-	-- function changePrice(ply, said)
-	-- 	if (self:IsValid()) then
-	-- 		if (ply == self:Getowning_ent()) then
-	-- 			said_check = said.sub(said, 0, 11)
-	-- 			if (said_check == "/kioskprice") then
-	-- 				changed_price = said.sub(said, 12)
-	-- 				local changed_price = tonumber(changed_price)
-	-- 				local check_price = isnumber(changed_price)
-	-- 				if (check_price == true) then
-	-- 					if (changed_price < 0) then
-	-- 						ply:ChatPrint("Nice try! Use a positive number.")
-	-- 					else
-	-- 						if (changed_price < 5001) then
-	-- 						ply:ChatPrint("Kiosk price changed to $" .. changed_price .. "!")
-	-- 						self:Setkiosk_price( changed_price )
-	-- 					else
-	-- 						ply:ChatPrint("Sorry, the maximum amount you can charge is $5,000.")
-	-- 					end
-	-- 				end
-	-- 					return ""
-	-- 				end
-	-- 			end
-	-- 		end
-	-- 	end
-	-- end
-	-- hook.Add("PlayerSay", "changePrice", changePrice)
 end
 
 function allowPickUp(ply, ent)
